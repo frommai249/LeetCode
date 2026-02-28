9d090ce0bea8e0782454dc35ed82758a281b7934
frommai249 <frommai249@gmail.com>
2026-02-28 08:55:36 +0700

    added 203

---
+++ Easy/203RemoveLinkedListElements.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    internal class _203RemoveLinkedListElements
+    {
+        class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            ListNode res = new ListNode(0, head);
+            ListNode current = res;
+
+            while (current.next != null)
+            {
+                if (current.next.val == val)
+                {
+                    current.next = current.next.next;
+                }
+                else
+                {
+                    current = current.next;
+                }
+            }
+
+            return res.next;
+        }
+    }
+}
